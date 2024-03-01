a90be645020ee6401c090dc99874ebed028c5a20
Jackal_lzt <bkyarokun@gmail.com>
2024-03-01 15:26:07 +0800

    feat: mongodb & mongoose init

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
-/**
- * @param port: 服务器监听的端口号
- **/
-app.listen((port = 3000), () => {
-	console.log("server is running on port 3000");
-});
 
 /**
  * @param req: 接收到来自客户端的数据
@@ -15,3 +9,20 @@ app.listen((port = 3000), () => {
 app.get("/", (req, res) => {
 	res.send("hello from node api");
 });
+
+mongoose
+	.connect(
+		"mongodb+srv://admin:<password>@curddb.us3eww3.mongodb.net/?retryWrites=true&w=majority&appName=CurdDB"
+	)
+	.then(() => {
+		console.log("Connected to database");
+		/**
+		 * @param port: 服务器监听的端口号
+		 **/
+		app.listen((port = 3000), () => {
+			console.log("server is running on port 3000");
+		});
+	})
+	.catch((err) => {
+		console.log("Database connection failed", err);
+	});
