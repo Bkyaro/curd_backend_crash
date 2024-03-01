6083ddd90fd95d657757fdb070c45342a738af73
Jackal_lzt <bkyarokun@gmail.com>
2024-03-01 14:47:58 +0800

    feat: init express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('init')
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+	console.log("server is running on port 3000");
+});
+
+/**
+ * @param req: 接收到来自客户端的数据
+ * @param res: 向客户端返回的数据
+ **/
+app.get("/", (req, res) => {
+	res.send("hello from node api");
+});
