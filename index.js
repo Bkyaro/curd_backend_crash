47cf7d3131e3b4f983cf7db3c6ee8f1bec9da7ee
Jackal_lzt <bkyarokun@gmail.com>
2024-03-01 17:10:04 +0800

    feat: achieve modify api

--- index.js
+++ index.js
@@ -14,28 +14,31 @@ app.get("/", (req, res) => {
 	res.send("hello from node api");
 });
 
+/** 创建产品条目API */
 app.post("/api/products", async (req, res) => {
 	try {
 		// 将用户的数据传给mongoose Schema模块，并存储
 		const product = await Product.create(req.body);
 		// 返回 200 状态码和经过Schema生成后的数据
 		res.status(200).json(product);
-	} catch (err) {
+	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 });
 
+/** 获取所有产品条目API */
 app.get("/api/products", async (req, res) => {
 	try {
 		// 获取所有数据库内的产品
 		const products = await Product.find({});
 		// 返回数据
 		res.status(200).json(products);
-	} catch (err) {
+	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 });
 
+/** 搜索单个产品条目API */
 app.get("/api/product/:id", async (req, res) => {
 	try {
 		// 根据参数 id 查找条目
@@ -43,7 +46,26 @@ app.get("/api/product/:id", async (req, res) => {
 		const product = await Product.findById(id);
 		// 返回数据
 		res.status(200).json(product);
-	} catch (err) {
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+/** 更改产品信息 API */
+app.put("/api/product/:id", async (req, res) => {
+	try {
+		// 获取参数 id
+		const { id } = req.params;
+		// 寻找并更新产品，存储返回值
+		const product = await Product.findByIdAndUpdate(id, req.body);
+		// 如果返回值为 false，抛出 404 并提示未找到产品
+		if (!product) {
+			return res.status(404).json({ message: "Product not found" });
+		}
+
+		const updatedProduct = await Product.findById(id);
+		res.status(200).json(updatedProduct);
+	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 });
@@ -61,6 +83,6 @@ mongoose
 			console.log("server is running on port 3000");
 		});
 	})
-	.catch((err) => {
+	.catch((error) => {
 		console.log("Database connection failed", err);
 	});
